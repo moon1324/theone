d34587d5d3f0f2bfb3f4c8c50a1a98be2e07a8ad
moon1324 <michaelshouldhope@gmail.com>
2024-10-30 10:12:34 +0900

    docs: change name of project in server/controller/index.js

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,6 +1,6 @@
 const index = async (req, res) => {
     res.set({ "Content-Type": "text/html; charset=utf-8" });
-    res.end("<h1>Project Oneul</h1>");
+    res.end("<h1>Project THEONE</h1>");
 };
 
 export { index };
