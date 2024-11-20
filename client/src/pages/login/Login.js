e64978892be8914b2349ab64b3a94ce991972d5f
moon1324 <michaelshouldhope@gmail.com>
2024-11-21 04:14:05 +0900

    style: add style for Login.js, in progress

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,7 +2,25 @@ import React from "react";
 import S from "./style";
 
 const Login = () => {
-    return <div></div>;
+    return (
+        <S.LoginContainer>
+            <S.LoginLogoWrapper>
+                <img src={process.env.PUBLIC_URL + "global/images/new_logo.png"} alt="logo" />
+            </S.LoginLogoWrapper>
+            <S.LoginBoxWrapper>
+                <S.LoginDescriptionContainer>
+                    <S.LoginDescriptionTitle>LOGIN / JOIN</S.LoginDescriptionTitle>
+                    <S.LoginDescriptionContent>로그인 없이도 페이지에 접속 가능합니다!</S.LoginDescriptionContent>
+                    <S.LoginDescriptionContent>글을 쓰기 원한다면 로그인 또는 회원가입을 진행해주세요!</S.LoginDescriptionContent>
+                </S.LoginDescriptionContainer>
+                <S.LoginButtonsContainer>
+                    <S.LoginButton></S.LoginButton>
+                    또는
+                    <S.LoginButton></S.LoginButton>
+                </S.LoginButtonsContainer>
+            </S.LoginBoxWrapper>
+        </S.LoginContainer>
+    );
 };
 
 export default Login;
--- client/src/pages/login/style.js
+++ client/src/pages/login/style.js
@@ -16,4 +16,51 @@ const S = {};
 //     /* 데스크탑용 스타일 */
 // }
 
+S.LoginContainer = styled.div`
+    width: 100%;
+    /* height: 100%; */
+    background-color: ${theme.PALETTE.background};
+    ${flexCenterColumn}
+`;
+
+S.LoginLogoWrapper = styled.div`
+    margin: 4rem;
+    width: 48rem;
+    ${flexCenter}
+    & img {
+        width: 100%;
+    }
+
+    @media only screen and (min-width: 768px) and (max-width: 1024px) {
+        margin: 6rem;
+        width: 36rem;
+        ${flexCenter}
+        &img {
+            width: 100%;
+        }
+    }
+`;
+
+S.LoginBoxWrapper = styled.div`
+    ${flexCenterColumn}
+    text-align: center;
+`;
+
+S.LoginDescriptionContainer = styled.div``;
+
+S.LoginDescriptionTitle = styled.h3`
+    margin: 1rem;
+    font-weight: 500;
+`;
+
+S.LoginDescriptionContent = styled.p`
+    margin: 0.5rem;
+`;
+
+S.LoginButtonsContainer = styled.div`
+    ${flexCenter}
+`;
+
+S.LoginButton = styled.div``;
+
 export default S;
