2be52f8a245bfc2f56214878b259bea46661c7e3
moon1324 <michaelshouldhope@gmail.com>
2024-12-12 14:45:08 +0900

    feat: add Suggestion.js and style.js files

---
+++ client/src/pages/suggestion/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import theme from "../../global/theme";
+import { flexCenter, flexCenterColumn } from "../../global/common";
+
+const S = {};
+
+// @media only screen and (max-width: 767px) {
+//     /* 모바일용 스타일 */
+// }
+
+// @media only screen and (min-width: 768px) and (max-width: 1024px) {
+//     /* 태블릿용 스타일 */
+// }
+
+// @media only screen and (min-width: 1025px) {
+//     /* 데스크탑용 스타일 */
+// }
+
+export default S;
